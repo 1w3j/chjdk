e86194e1126f1cff0836964ec2e1e2c261d601f8
1w3j <iqbal_cs98@hotmail.com>
2020-10-03 11:27:01 +0800

    Added OpenJFX support

--- chjdk.sh
+++ chjdk.sh
@@ -7,11 +7,13 @@
 source ~/1w3j/functions.sh
 
 JDK_PATH=/opt/jdk
+JFX_PATH=/opt/openjfx
 JAVA_BIN_PATH=/usr/bin/java
 number=0
 
 msg "Using \`java\` path: ${JAVA_BIN_PATH}"
 msg "Using JDK folder path: ${JDK_PATH}"
+msg "Using OpenJFX folder path: ${JFX_PATH}"
 
 print_usage() {
     cat <<EOF
@@ -50,10 +52,12 @@ chjdk(){
                 # Checking if ${number} has actually a numeric value and that the 'number' passed is actually in the list
                 if [[ "${1}" =~ ^[0-9]+$ ]] && [[ "${1}" -le ${#jdks[@]} ]]; then
                     selected_jdk=${jdks[$((${1}-1))]}
+                    selected_jdk_version=$(basename ${selected_jdk})
                     full_jdk_bin_path_array=("${selected_jdk}"/*/bin/java) # the array () expands the glob
                     full_jdk_bin_path="$(printf "%s ${full_jdk_bin_path_array[*]}")"
-                    [[ -e ${JAVA_BIN_PATH} ]] && warn "Removing ${JAVA_BIN_PATH}" && sudo rm -i ${JAVA_BIN_PATH}
                     [[ -e /opt/defaultjdk ]] && warn "Removing /opt/defaultjdk" && sudo rm -i /opt/defaultjdk
+                    [[ -e /opt/defaultjfx ]] && warn "Removing /opt/defaultjfx" && sudo rm -i /opt/defaultjfx
+                    [[ -e ${JAVA_BIN_PATH} ]] && warn "Removing ${JAVA_BIN_PATH}" && sudo rm -i ${JAVA_BIN_PATH}
                     # if sudo rm -i was successful or JAVA_BIN_PATH was already externally removed, then proceed
                     # shellcheck disable=SC2181
                     if [[ ${?} -eq 0 ]] || [[ ! -e ${JAVA_BIN_PATH} ]]; then
@@ -62,6 +66,12 @@ chjdk(){
                         sudo ln -fs "${full_jdk_bin_path}" ${JAVA_BIN_PATH}
                         msg Linking \""${selected_jdk}"\" to \"/opt/defaultjdk\"
                         sudo ln -fs "${selected_jdk}"/*/ /opt/defaultjdk
+                        if [[ -e "${JFX_PATH}/${selected_jdk_version}" ]]; then
+                            msg Linking \""${JFX_PATH}/${selected_jdk_version}"\" to \"/opt/defaultjfx\"
+                            sudo ln -fs "${JFX_PATH}/${selected_jdk_version}"/*/lib /opt/defaultjfx
+                        else
+                            warn "Selected JDK respective OpenJFX release was not found on ${JFX_PATH}/${selected_jdk_version}"
+                        fi
                         msg "Job Finished"
                         java -version
                     else
